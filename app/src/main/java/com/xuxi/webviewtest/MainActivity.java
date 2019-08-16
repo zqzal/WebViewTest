942782a81a98826ebe65a9f505f5186bcf62483f
zqzal <936952020@qq.com>
2019-08-16 10:11:52 +0800

    webview add

--- app/src/main/java/com/xuxi/webviewtest/MainActivity.java
+++ app/src/main/java/com/xuxi/webviewtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.xuxi.webviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient()); //当需要从一个网页跳转到另一个网页时，我们希望目标网页仍然
+        //在当前Webview显示，而不是打开系统浏览器
+        webView.loadUrl("https://www.baidu.com");
+
     }
 }
